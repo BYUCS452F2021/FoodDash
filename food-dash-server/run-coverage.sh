0838a63d2c6255d7f04ebce89a2572a5fa81f4e3
Sam Meng <sm959@byu.edu>
2021-09-20 19:27:48 -0600

    Update coverage script with correct directory

--- food-dash-server/run-coverage.sh
+++ food-dash-server/run-coverage.sh
@@ -1,5 +1,5 @@
 
-export CC_TEST_REPORTER_ID=038c872feefaf775807be55fe2cf4138b7b523b7e72a76e5e63f561a616d2e0d
+export CC_TEST_REPORTER_ID=test
 export JACOCO_SOURCE_PATH=src/main/java
 printenv
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
