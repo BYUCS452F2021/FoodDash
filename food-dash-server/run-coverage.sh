4a876b70ee2712d5dc1fbab23041f48d91d77170
Sam Meng <sm959@byu.edu>
2021-09-20 21:55:44 -0600

    Add changes to build and deployment

--- food-dash-server/run-coverage.sh
+++ food-dash-server/run-coverage.sh
@@ -1,5 +1,5 @@
 
-export CC_TEST_REPORTER_ID=test
+export CC_TEST_REPORTER_ID=UPDATE_THIS_WITH_REPORTER_ID
 export JACOCO_SOURCE_PATH=src/main/java
 printenv
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
@@ -10,4 +10,5 @@ mvn --no-transfer-progress clean verify package
 
 ./cc-test-reporter format-coverage target/site/jacoco/jacoco.xml --input-type jacoco
 
-./cc-test-reporter upload-coverage
+# don't try to output coverage until the CC_TEST_REPORTER_ID is set properly
+# ./cc-test-reporter upload-coverage
