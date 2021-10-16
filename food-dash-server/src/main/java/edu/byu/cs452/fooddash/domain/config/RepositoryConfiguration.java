cdf95adfd57b31fa0787ea2d0ca7a6897ddfb3d0
SzuJanLin <szujan.lin@gmail.com>
2021-10-16 15:34:04 -0600

    solve rebase conflict

--- food-dash-server/src/main/java/edu/byu/cs452/fooddash/domain/config/RepositoryConfiguration.java
+++ food-dash-server/src/main/java/edu/byu/cs452/fooddash/domain/config/RepositoryConfiguration.java
@@ -6,21 +6,20 @@ import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 @Configuration
 @EnableR2dbcRepositories
 class RepositoryConfiguration {
-  //  extends AbstractR2dbcConfiguration {
+//  extends AbstractR2dbcConfiguration {
 
-  //     @Value("${db.connection}")
-  //     private String connectionString;
+//     @Value("${db.connection}")
+//     private String connectionString;
 
-  //     @Override
-  //     @Bean
-  //     public ConnectionFactory connectionFactory() {
-  //         // connectionString looks like this:
-  //         // r2dbc:gcp:postgres://user:123456@my-project:us-central1:r2dbctest/testdb
-  //         ConnectionFactory connectionFactory = ConnectionFactories.get(connectionString);
-  //         ConnectionPoolConfiguration configuration =
-  // ConnectionPoolConfiguration.builder(connectionFactory)
-  //                 .maxIdleTime(Duration.ofMillis(1000)).maxSize(10).build();
+//     @Override
+//     @Bean
+//     public ConnectionFactory connectionFactory() {
+//         // connectionString looks like this:
+//         // r2dbc:gcp:postgres://user:123456@my-project:us-central1:r2dbctest/testdb
+//         ConnectionFactory connectionFactory = ConnectionFactories.get(connectionString);
+//         ConnectionPoolConfiguration configuration = ConnectionPoolConfiguration.builder(connectionFactory)
+//                 .maxIdleTime(Duration.ofMillis(1000)).maxSize(10).build();
 
-  //         return new ConnectionPool(configuration);
-  //     }
+//         return new ConnectionPool(configuration);
+//     }
 }
